e400f14e9012cda60d9ecccbabe8b021182a3c6a
nakachi <nakachi@ad-scope.co.jp>
2024-08-31 13:35:33 +0900

    データベースの追加

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import './App.css'
-import '@aws-amplify/ui-react/styles.css'
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
 
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import { Amplify } from 'aws-amplify'
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-import aws_exports from './aws-exports'
-import logo from './logo.svg'
+import aws_exports from "./aws-exports";
+import { DynamoDB } from "./components/DynamoDB"; //ここを追加
 
-Amplify.configure(aws_exports)
+Amplify.configure(aws_exports);
 
-function App () {
+function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+    <div className="App">
+      <header className="App-header">
+        {/* 下記を変更 */}
+        <DynamoDB />
       </header>
     </div>
-  )
+  );
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
---
+++ src/components/DynamoDB.jsx
@@ -0,0 +1,92 @@
+import { generateClient } from 'aws-amplify/api';
+
+import { useEffect, useState } from 'react'
+
+import { createTodo } from '../graphql/mutations'
+import { listTodos } from '../graphql/queries'
+
+const API = generateClient();
+
+export function DynamoDB () {
+  const [todos, setTodos] = useState([])
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  async function fetchTodos () {
+    try {
+      const response = await API.graphql({
+        query: listTodos
+      })
+      const todosData = response.data.listTodos.items
+      setTodos(todosData)
+    } catch (err) {
+      console.log('error fetching books...', err)
+    }
+  }
+
+  const handleTitleChange = event => {
+    setTitle(event.target.value)
+  }
+
+  const handleDescriptionChange = event => {
+    setDescription(event.target.value)
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      const todoDetails = {
+        owner: '37744ac8-7001-70b3-dd78-ea8dd01b5903',
+        title,
+        description
+      }
+      console.log(todoDetails)
+      const newTodo = await API.graphql({
+        query: createTodo,
+        variables: { input: todoDetails },
+        authMode: 'userPool'
+      })
+      console.log('投稿しました。', newTodo)
+      alert('投稿に成功しました。')
+      fetchTodos()
+    } catch (error) {
+      console.error('投稿に失敗しました。', error)
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type='text' value={title} onChange={handleTitleChange} />
+        </label>
+        <br />
+        <label>
+          Description:
+          <input
+            type='text'
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+        </label>
+        <br />
+        <button type='submit'>Create Post</button>
+      </form>
+      <ol>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <p>
+              title={todo.title}, description={todo.description}
+            </p>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
+}
